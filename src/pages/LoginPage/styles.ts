6e994fb464e406fb17c53c132067e0e7e687cccb
Alexander Borodin <borodinag1902@gmail.com>
2025-11-02 16:11:39 +0200

    feat: update palette and change properties in styles

--- src/pages/LoginPage/styles.ts
+++ src/pages/LoginPage/styles.ts
@@ -5,12 +5,8 @@ import { theme } from '@/theme';
 export const PageWrapper = styled.div`
   display: flex;
   flex-direction: column;
-  min-height: 100vh;
   padding: 24px;
-  @media (min-width: 1200px) {
-    padding: 48px;
-  }
-  background: ${theme.palette.loginColors.pageWrapperBg};
+  background: ${theme.palette.backgrounds.white};
   background: ${theme.palette.backgrounds.mainGradient};
   justify-content: space-between;
 `;
